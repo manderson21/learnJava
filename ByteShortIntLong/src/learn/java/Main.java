22bff33d843a55e459dc3166c49ccb0f1f48080b
Matthew Anderson <49250378+manderson21@users.noreply.github.com>
2020-02-27 12:04:33 -0600

    Practicing with numerical primitive data types

---
+++ ByteShortIntLong/src/learn/java/Main.java
@@ -0,0 +1,17 @@
+package learn.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10_000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println(myMaxIntValue + 2);
+
+        byte myMinByteValue = By
+    }
+}
