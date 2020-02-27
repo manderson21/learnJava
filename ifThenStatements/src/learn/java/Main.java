215010598a9d8e01139fbeede68938a2514e06a0
Matthew Anderson <49250378+manderson21@users.noreply.github.com>
2020-02-27 12:10:22 -0600

    Introduced to control flow statement: if-then

---
+++ ifThenStatements/src/learn/java/Main.java
@@ -0,0 +1,13 @@
+package learn.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean isAlien = true;
+        if (isAlien) System.out.println("It is an alien!");
+        else System.out.println("It is not an alien!");
+
+        int topScore = 100;
+        if (topScore <= 100) System.out.println("You go the high score!");
+    }
+}
